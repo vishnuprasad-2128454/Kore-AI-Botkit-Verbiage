7c75d2f628f84d2065a4b119b87f9fc6c0ff5b3c
dante851 <vignesh.s98@cognizant.com>
2024-11-22 13:09:44 +0530

    Update SimpleConversationalBot.js

--- SimpleConversationalBot.js
+++ SimpleConversationalBot.js
@@ -1,10 +1,10 @@
 var botId = "st-5ce0721c-5c5e-56b0-a0ba-cac19303c9a5";
 var botName = "OrderManage";
 var sdk = require("./lib/sdk");
-const xlsx = require('node-xlsx').default;
-const fs = require('fs');
-const {populateBotResponse} = require('./utility');
-const {msgTemplate} = require('./utility');
+const xlsx = require("node-xlsx").default;
+const fs = require("fs");
+const { populateBotResponse } = require("./utility");
+const { msgTemplate } = require("./utility");
 /*
  * This is the most basic example of BotKit.
  *
@@ -13,49 +13,51 @@ const {msgTemplate} = require('./utility');
  * We can either update the message, or chose to call one of 'sendBotMessage' or 'sendUserMessage'
  */
 module.exports = {
-    botId   : botId,
-    botName : botName,
+  botId: botId,
+  botName: botName,
 
-    on_user_message : function(requestId, data, callback) {
-        console.log("user",data.message)
-        if (data.message === "Hi") {
-            data.message = "Hello";
-            console.log("user message",data.message);
-            //Sends back 'Hello' to user.
-            return sdk.sendUserMessage(data, callback);
-        } else if(!data.agent_transfer){
-            //Forward the message to bot
-            return sdk.sendBotMessage(data, callback);
-        } else {
-            data.message = "Agent Message";
-            return sdk.sendUserMessage(data, callback);
-        }
-    },
-    on_bot_message  : function(requestId, data, callback) {
-        if (data.message === 'hi') {
-            data.message = 'The Bot says hello!';
-            console.log("bot message",data.message)
-        }
-        console.log("verbiage_builder_resp",verbiage_En_RespData);
-        //Sends back the message to user
-         const currentLanguage = context.currentLanguage;
-        const verbiageBuilderData = currentLanguage === 'en' ? verbiage_En_RespData : verbiage_Fr_RespData;
-       data.message = populateBotResponse(verbiageBuilderData,data.message,data.context.session.BotUserSession);
-        console.log("bot message",data.message)
-        return sdk.sendUserMessage(data, callback);
-    },
-    on_agent_transfer : function(requestId, data, callback){
-        return callback(null, data);
-    },
-    on_event : function (requestId, data, callback) {
-        console.log("on_event -->  Event : ", data.message);
-        return callback(null, data);
-    },
-    on_alert : function (requestId, data, callback) {
-        console.log("on_alert -->  : ", data, data.message);
-        return sdk.sendAlertMessage(data, callback);
+  on_user_message: function (requestId, data, callback) {
+    console.log("user", data.message);
+    if (data.message === "Hi") {
+      data.message = "Hello";
+      console.log("user message", data.message);
+      //Sends back 'Hello' to user.
+      return sdk.sendUserMessage(data, callback);
+    } else if (!data.agent_transfer) {
+      //Forward the message to bot
+      return sdk.sendBotMessage(data, callback);
+    } else {
+      data.message = "Agent Message";
+      return sdk.sendUserMessage(data, callback);
     }
-
+  },
+  on_bot_message: function (requestId, data, callback) {
+    if (data.message === "hi") {
+      data.message = "The Bot says hello!";
+      console.log("bot message", data.message);
+    }
+    console.log("verbiage_builder_resp", verbiage_En_RespData);
+    //Sends back the message to user
+    const currentLanguage = context.currentLanguage;
+    const verbiageBuilderData =
+      currentLanguage === "en" ? verbiage_En_RespData : verbiage_Fr_RespData;
+    data.message = populateBotResponse(
+      verbiageBuilderData,
+      data.message,
+      data.context.session.BotUserSession
+    );
+    console.log("bot message", data.message);
+    return sdk.sendUserMessage(data, callback);
+  },
+  on_agent_transfer: function (requestId, data, callback) {
+    return callback(null, data);
+  },
+  on_event: function (requestId, data, callback) {
+    console.log("on_event -->  Event : ", data.message);
+    return callback(null, data);
+  },
+  on_alert: function (requestId, data, callback) {
+    console.log("on_alert -->  : ", data, data.message);
+    return sdk.sendAlertMessage(data, callback);
+  },
 };
-
-
