cb7d34359d7023180bb9c164811740cb307d92ff
dante851 <vignesh.s98@cognizant.com>
2024-11-22 12:54:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var config = require("./config");
 
 var app = new Application(null, config);
 var server = new Server(config, app);
-const verbiageBuilder = require("./verbiageBuilder");
+const { verbiageBuilder } = require("./verbiageBuilder");
 sdk.checkNodeVersion();
 
 verbiage_En_RespData = verbiageBuilder("ESI_PHA_BOT_RESP_BUILDER_EN_CA.xlsx");
-verbiage_Fr_RespData = verbiageBuilder("ESI_PHA_BOT_RESP_BUILDER_FR_CA.xlsx");  
+verbiage_Fr_RespData = verbiageBuilder("ESI_PHA_BOT_RESP_BUILDER_FR_CA.xlsx");
 
 server.start();
 
