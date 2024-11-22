d4612a3796e2ad018aa2e28fdd886357860c65a6
dante851 <vignesh.s98@cognizant.com>
2024-11-22 12:26:21 +0530

    Update verbiageBuilder.js

--- verbiageBuilder.js
+++ verbiageBuilder.js
@@ -1,6 +1,7 @@
 const xlsx = require("node-xlsx");
 
-function verbiageBuilder(respExcelFile) {
+module.exports = {
+    verbiageBuilder: function(respExcelFile) {
     const workbook = xlsx.parse(`./${respExcelFile}`);
     let verbiageResponse = [];
 
@@ -21,25 +22,4 @@ function verbiageBuilder(respExcelFile) {
     return verbiageResponse;
 }
 
-// function verbiageBuilder() {
-//     const workbook = xlsx.parse(`./ESI_PHA_BOT_RESP_BUILDER_EN_CA.xlsx`);
-//     let verbiageResponse = [];
-
-//     for (const element in workbook) {
-//         const sheet = workbook[element].data;
-//         const headers = sheet[0];
-
-//         let sheetData = sheet.slice(1).map(row => {
-//             let obj = {};
-//             headers.forEach((header, index) => {
-//                 obj[header] = row[index] ? row[index] : null;
-//             });
-//             return obj;
-//         });
-
-//         verbiageResponse.push(...sheetData);
-//     }
-//     return verbiageResponse;
-// }
-
-module.exports = verbiageBuilder;
+}
